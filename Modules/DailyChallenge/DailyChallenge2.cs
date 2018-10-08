8e553c4762de1d22d5c69cbd193ba695fd6b683e
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-08 23:00:27 +0200

    DailyChallenge3

---
+++ Modules/DailyChallenge/DailyChallenge2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge2
+    {
+        /*
+         * Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+You can modify the input array in-place.
+         */
+
+        public int Proceed(int[] input)
+        {
+            int index = 0;
+            foreach (int value in input)
+            {
+                CorrectValue(value, input, index++);
+            }
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (input[i] == i + 1)
+                {
+                    continue;
+                }
+                return i + 1;
+            }
+            return input.Length + 1;
+        }
+
+        private void CorrectValue(int value, int[] input, int previousIndex)
+        {
+            if (value <= 0 || value > input.Length || input[value - 1] == value)
+            {
+                return;
+            }
+            int index = value - 1;
+            int temp = input[index];
+            int tempIndex = temp - 1;
+            if (tempIndex == previousIndex)
+            {
+                // Break infinity loop caused by changing the same pair
+                input[index] = value;
+                input[previousIndex] = temp;
+                return;
+            }
+            if (temp > 0 && temp <= input.Length)
+            {
+                // Do not replace importent data
+                CorrectValue(temp, input, tempIndex);
+            }
+            input[index] = value;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge2.cs
@@ -0,0 +1,28 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge2
+    {
+        [TestMethod]
+        public void DailyChallenge2()
+        {
+            DailyChallenge2 model = new DailyChallenge2();
+            Assert.AreEqual(2, model.Proceed(new int[] { 3, 4, -1, 1 }));
+            Assert.AreEqual(3, model.Proceed(new int[] { 1, 2, 0 }));
+            Assert.AreEqual(4, model.Proceed(new int[] { 1, 3, 2, 7, 6, -2, 5, -123, 24, 1, 5, 0 }));
+            Assert.AreEqual(5, model.Proceed(new int[] { 3, 2, 4, -1, 1 }));
+            Assert.AreEqual(5, model.Proceed(new int[] { 1, 2, 3, 4 }));
+            Assert.AreEqual(1, model.Proceed(new int[] { -1, 4, 4, -4 }));
+            Assert.AreEqual(1, model.Proceed(new int[] { 2, 2, 2, 2 }));
+            Assert.AreEqual(1, model.Proceed(new int[] { 3, 2, 3, 2 }));
+        }
+    }
+}
