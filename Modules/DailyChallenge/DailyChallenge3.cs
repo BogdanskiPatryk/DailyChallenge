11a24ff9051262f706e41ea58c18b03bf75dfccd
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-09 20:42:37 +0200

    Daily3

---
+++ Modules/DailyChallenge/DailyChallenge3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge3
+    {
+        /*
+         * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass?
+
+         */
+
+        public bool Proceed(int[] input, int toCheck)
+        {
+            HashSet<int> already = new HashSet<int>();
+            foreach (int value in input)
+            {
+                int sub = toCheck - value;
+                if (already.Contains(sub))
+                {
+                    return true;
+                }
+                already.Add(value);
+            }
+            return false;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge3.cs
@@ -0,0 +1,30 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge3
+    {
+        [TestMethod]
+        public void DailyChallenge3()
+        {
+            DailyChallenge3 obj = new DailyChallenge3();
+            Assert.IsTrue(obj.Proceed(new int[] { 10, 15, 3, 7 }, 17));
+            Assert.IsTrue(obj.Proceed(new int[] { 10, 15, 3, 7 }, 13));
+            Assert.IsTrue(obj.Proceed(new int[] { 10, 15, 3, 7 }, 10));
+            Assert.IsFalse(obj.Proceed(new int[] { 10, 15, 3, 7 }, 14));
+            Assert.IsFalse(obj.Proceed(new int[] { 10, 15, 3, 7 }, 7));
+            Assert.IsTrue(obj.Proceed(new int[] { -5, 15, 3, 7 }, 2));
+
+            Assert.IsTrue(obj.Proceed(new int[] { 1, 1, 1, 5 }, 6));
+            Assert.IsTrue(obj.Proceed(new int[] { 1, 1, 1, 5 }, 2));
+            Assert.IsFalse(obj.Proceed(new int[] { 1, 1, 1, 5 }, 3));
+        }
+    }
+}
