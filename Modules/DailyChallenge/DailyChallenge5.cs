14ca8d91a2c7c722f9ed7cac36d9408fee8e012a
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-11 22:05:25 +0200

    Daily 5

---
+++ Modules/DailyChallenge/DailyChallenge5.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge5
+    {
+        private class Node
+        {
+            public int Element;
+            public int Both;
+        }
+
+        private int _firstAddress = -1;
+        private int _lastAddress = -1;
+
+        public void Add(int element)
+        {
+            Node first = DereferencePointer(_firstAddress);
+            if (first == null)
+            {
+                // First element
+                first = new Node() { Element = element, Both = 0 };
+                _firstAddress = GetPointer(first);
+                _lastAddress = _firstAddress;
+                return;
+            }
+            // Add
+            Node newNode = new Node() { Element = element, Both = _lastAddress };
+            Node last = DereferencePointer(_lastAddress);
+            last.Both = _lastAddress ^ GetPointer(newNode);
+            _lastAddress = GetPointer(newNode);
+        }
+
+        public int Get(int index)
+        {
+            if (index < 0 || _firstAddress == -1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int i = 0;
+            int currentAdd = _firstAddress;
+            while (true)
+            {
+                Node currentNode = DereferencePointer(currentAdd);
+                if (i++ == index)
+                {
+                    return currentNode.Element;
+                }
+                if (currentAdd == _lastAddress)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                currentAdd = currentAdd ^ currentNode.Both;
+            }
+        }
+
+        #region memory
+
+        private List<Node> _memoryAccess = new List<Node>();
+
+        private int GetPointer(Node node)
+        {
+            int index = _memoryAccess.IndexOf(node);
+            if (index < 0)
+            {
+                _memoryAccess.Add(node);
+                index = _memoryAccess.Count - 1;
+            }
+            return index;
+        }
+
+        private Node DereferencePointer(int pointer)
+        {
+            if (pointer < 0 || pointer >= _memoryAccess.Count)
+            {
+                return null;
+            }
+            return _memoryAccess[pointer];
+        }
+
+        #endregion memory
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge5.cs
@@ -0,0 +1,29 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge5
+    {
+        [TestMethod]
+        public void DailyChallenge5()
+        {
+            DailyChallenge5 daily = new DailyChallenge5();
+            daily.Add(12);
+            daily.Add(15);
+            daily.Add(27);
+            daily.Add(89);
+
+            Assert.AreEqual(12, daily.Get(0));
+            Assert.AreEqual(15, daily.Get(1));
+            Assert.AreEqual(27, daily.Get(2));
+            Assert.AreEqual(89, daily.Get(3));
+        }
+    }
+}
