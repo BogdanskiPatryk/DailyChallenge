20d3738240651a17b791025b766acf3b4d05b2ea
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-11 19:37:15 +0200

    Daily4

---
+++ Modules/DailyChallenge/DailyChallenge4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge4
+    {
+        /*
+         * cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+Given this implementation of cons:
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+         */
+
+        public Tuple<int, int> Cons(int a, int b)
+        {
+            return new Tuple<int, int>(a, b);
+        }
+
+        public int Car(Tuple<int, int> pair)
+        {
+            return pair.Item1;
+        }
+
+        public int Cdr(Tuple<int, int> pair)
+        {
+            return pair.Item2;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge4.cs
@@ -0,0 +1,22 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge4
+    {
+        [TestMethod]
+        public void DailyChallenge4()
+        {
+            DailyChallenge4 daily = new DailyChallenge4();
+            Assert.AreEqual(3, daily.Car(daily.Cons(3, 4)));
+            Assert.AreEqual(4, daily.Cdr(daily.Cons(3, 4)));
+        }
+    }
+}
