479124652f3db0cb416b35ccd060fc14c8cc203d
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-14 20:20:36 +0200

    Daily 7

---
+++ Modules/DailyChallenge/DailyChallenge7.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge7
+    {
+        /*
+         *
+         * A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+Given the root to a binary tree, count the number of unival subtrees.
+For example, the following tree has 5 unival subtrees:
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+
+         */
+
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public int Proceed(Node treeRoot)
+        {
+            return ProceedIntern(treeRoot, null, 1);
+        }
+
+        private int ProceedIntern(Node node, Node parent, int multipler)
+        {
+            int result = 0;
+            if (node == null)
+            {
+                return result;
+            }
+            int newMultipler = 1;
+            if (parent != null && parent.Value == node.Value)
+            {
+                result += multipler;
+                newMultipler = multipler + 1;
+            }
+            result += ProceedIntern(node.Left, node, newMultipler);
+            result += ProceedIntern(node.Right, node, newMultipler);
+            return result;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge7.cs
@@ -0,0 +1,60 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge7;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge7
+    {
+        [TestMethod]
+        public void DailyChallenge7()
+        {
+            DailyChallenge7 daily = new DailyChallenge7();
+            Node root1 = new Node()
+            {
+                Value = 0,
+                Left = new Node() { Value = 1 },
+                Right = new Node()
+                {
+                    Value = 0,
+                    Right = new Node() { Value = 0 },
+                    Left = new Node()
+                    {
+                        Value = 1,
+                        Right = new Node() { Value = 1 },
+                        Left = new Node() { Value = 1 }
+                    }
+                }
+            };
+            Node root2 = new Node()
+            {
+                Value = 0,
+                Left = new Node() { Value = 0 },
+                Right = new Node()
+                {
+                    Value = 0,
+                    Right = new Node() { Value = 0 },
+                    Left = new Node()
+                    {
+                        Value = 0,
+                        Right = new Node() { Value = 1 },
+                        Left = new Node()
+                        {
+                            Value = 1,
+                            Right = new Node { Value = 1 },
+                            Left = new Node { Value = 1 },
+                        }
+                    }
+                }
+            };
+            Assert.AreEqual(5, daily.Proceed(root1));
+            Assert.AreEqual(8, daily.Proceed(root2));
+        }
+    }
+}
