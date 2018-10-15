7f68163d97323a3201b69f86c05aed323b770afb
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-15 21:01:59 +0200

    daily 9

---
+++ Modules/DailyChallenge/DailyChallenge9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    /*
+     * Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+     */
+
+    public class DailyChallenge9
+    {
+        public async Task Proceed(Action action, int delayMS, CancellationToken token)
+        {
+            if (delayMS < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            await Task.Delay(delayMS, token);
+            action();
+        }
+
+        public Task Proceed2(Action action, int delayMS, CancellationToken token)
+        {
+            if (delayMS < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(delayMS, token);
+                action();
+            }, token);
+        }
+    }
+}
