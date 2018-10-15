e2066c5a7923258a6208d5feda0640aa4d3272bd
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-15 20:41:03 +0200

    daily 8

---
+++ Modules/DailyChallenge/DailyChallenge8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge8
+    {
+        /*
+         * Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+         */
+
+        public int Proceed(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            return ProceedInternal(array);
+        }
+
+        private int ProceedInternal(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            if (array.Length == 1)
+            {
+                return array[0];
+            }
+            if (array.Length == 2)
+            {
+                return Math.Max(array[0], array[1]);
+            }
+            int sumFirst = array[0] + ProceedInternal(array.Skip(2).ToArray());
+            int sumSecond = array[1] + ProceedInternal(array.Skip(3).ToArray());
+            return Math.Max(sumFirst, sumSecond);
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge8.cs
@@ -0,0 +1,25 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge8
+    {
+        [TestMethod]
+        public void DailyChallenge8()
+        {
+            DailyChallenge8 daily = new DailyChallenge8();
+            Assert.AreEqual(13, daily.Proceed(new int[] { 2, 4, 6, 2, 5 }));
+            Assert.AreEqual(10, daily.Proceed(new int[] { 5, 1, 1, 5 }));
+            Assert.AreEqual(3, daily.Proceed(new int[] { 1, 1, 1, 1, 1, 1 }));
+            Assert.AreEqual(9, daily.Proceed(new int[] { 1, 2, 3, 4, 5, -6 }));
+            Assert.AreEqual(9, daily.Proceed(new int[] { 0, 0, 3, 0, 0, 3, 0, 0, 3 }));
+        }
+    }
+}
