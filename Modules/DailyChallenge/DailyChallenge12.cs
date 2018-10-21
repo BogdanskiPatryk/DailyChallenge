c35879db50d417dd627b0fd09b854cdf0415745a
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-21 18:54:14 +0200

    Daily12

---
+++ Modules/DailyChallenge/DailyChallenge12.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge12
+    {
+
+        public string Proceed(string str, int k)
+        {
+            List<string> result = new List<string>();
+            for(int i = 0; i< str.Length; ++i)
+            {
+                string subString = str.Substring(i);
+                if (result.Any() && result.Select(x => x.Length).Max() >= subString.Length)
+                {
+                    break;
+                }
+                string strResult = ProceedIntern(subString, k);
+                if (string.IsNullOrEmpty(strResult) == false)
+                {
+                    result.Add(strResult);
+                }
+            }
+            return result.OrderByDescending(x => x.Length).FirstOrDefault();
+        }
+
+        private string ProceedIntern(string str, int k)
+        {
+            int left = k;
+            HashSet<char> chars = new HashSet<char>();
+            StringBuilder sb = new StringBuilder();
+            foreach (char ch in str)
+            {
+                if (chars.Add(ch))
+                {
+                    left--;
+                    if (left < 0)
+                    {
+                        break;
+                    }
+                }
+                sb.Append(ch);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge12.cs
@@ -0,0 +1,22 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge12
+    {
+        [TestMethod]
+        public void DailyChallenge12()
+        {
+            DailyChallenge12 daily = new DailyChallenge12();
+            Assert.AreEqual("bcb", daily.Proceed("abcba", 2));
+            Assert.AreEqual("jasfjaf", daily.Proceed("lijasfjafwlajsd", 4));
+        }
+    }
+}
