af5f505b51144f5af1df78c2cdba80a33483a9d7
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-22 21:20:25 +0200

    Daily 13

---
+++ Modules/DailyChallenge/DailyChallenge13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge13
+    {
+        public double Proceed(int n)
+        {
+            Random random = new Random();
+            int inCircle = 0;
+            for(int i = 0; i <n;++i)
+            {
+                double x = random.NextDouble() * 2 - 1;
+                double y = random.NextDouble() * 2 - 1;
+                if (Math.Pow(x,2) + Math.Pow(y,2) <= 1)
+                {
+                    inCircle++;
+                }
+            }
+            return 4 * (double)inCircle / n;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge13.cs
@@ -0,0 +1,35 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge13
+    {
+        [TestMethod]
+        public void DailyChallenge13()
+        {
+            DailyChallenge13 daily = new DailyChallenge13();
+            int[] ks = new int[]
+                {
+                    10,
+                    100,
+                    1000,
+                    10000,
+                    100000,
+                    1000000,
+                };
+            foreach(int k in ks)
+            {
+                double result = daily.Proceed(k);
+                Trace.WriteLine($"{result} for {k} samples");
+            }
+        }
+    }
+}
