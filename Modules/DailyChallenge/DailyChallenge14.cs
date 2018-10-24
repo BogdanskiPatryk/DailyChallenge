8b296624af34e303d7f2ac3bf72049eb279c904f
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-24 20:41:41 +0200

    daily14

---
+++ Modules/DailyChallenge/DailyChallenge14.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge14
+    {
+        /*
+         * Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+         */
+
+        public int Proceed(Stream stream)
+        {
+            Random random = new Random();
+            int? result = null;
+            int lastRand = -1;
+            while(true)
+            {
+                int readed = stream.ReadByte();
+                if (readed == -1)
+                {
+                    if (result == null)
+                    {
+                        throw new ArgumentException();
+                    }
+                    return result.Value;
+                }
+                int rand = random.Next(0, int.MaxValue);
+                if (rand > lastRand)
+                {
+                    lastRand = rand;
+                    result = readed;
+                }
+            }
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge14.cs
@@ -0,0 +1,25 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge14
+    {
+        [TestMethod]
+        public void DailyChallenge14()
+        {
+            DailyChallenge14 daily = new DailyChallenge14();
+            using (Stream s = new MemoryStream(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }))
+            {
+                daily.Proceed(s);
+                    }
+        }
+    }
+}
