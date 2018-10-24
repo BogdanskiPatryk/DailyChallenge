a9cbd5a8cef86b62a61b6f3ee188f1336f6e296c
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-24 21:26:56 +0200

    Daily15

---
+++ Modules/DailyChallenge/DailyChallenge15.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge15
+    {
+        /*
+         * You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
+•	record(order_id): adds the order_id to the log
+•	get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
+You should be as efficient with time and space as possible.
+
+         */
+
+
+        public class Order
+        {
+            public Guid ID { get; set; }
+        }
+
+        LinkedList<Order> _orders = new LinkedList<Order>();
+        private readonly int _limit;
+        private int _records;
+
+        public DailyChallenge15(int n)
+        {
+            _limit = n;
+            _records = 0;
+        }
+
+        public void Record(Order order)
+        {
+            _records++;
+            _orders.AddFirst(order);
+            if (_records > _limit)
+            {
+                _orders.RemoveLast();
+            }
+        }
+
+        public Order[] GetLast(int i)
+        {
+            return _orders.Take(i).ToArray();
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge15.cs
@@ -0,0 +1,33 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge15;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge15
+    {
+        [TestMethod]
+        public void DailyChallenge15()
+        {
+            DailyChallenge15 daily = new DailyChallenge15(10);
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            Assert.AreEqual(4, daily.GetLast(10).Length);
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            daily.Record(new Order { ID = Guid.NewGuid() });
+            Assert.AreEqual(6, daily.GetLast(6).Length);
+            Assert.AreEqual(3, daily.GetLast(3).Length);
+            Guid lastGuid = Guid.NewGuid();
+            daily.Record(new Order { ID = lastGuid });
+            Assert.AreEqual(lastGuid, daily.GetLast(1)[0].ID);
+        }
+    }
+}
