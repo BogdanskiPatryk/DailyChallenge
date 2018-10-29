83d07f3ca1ca6e8ab9cc86ad8b88ffd914083eb8
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-29 20:59:35 +0100

    daily19

---
+++ Modules/DailyChallenge/DailyChallenge19.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge19
+    {
+        /*
+         * Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+In this example, assume nodes with the same value are the exact same node objects.
+Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+         */
+
+        public class Element
+        {
+            public Guid ID { get; private set; }
+            public int Value { get; private set; }
+
+            public Element(int value)
+            {
+                Value = value;
+                ID = Guid.NewGuid();
+            }
+        }
+
+        public Element Proceed(LinkedList<Element> first, LinkedList<Element> second)
+        {
+            int diff = Math.Abs(first.Count - second.Count);
+            LinkedList<Element> shortest,longest;
+            if (first.Count < second.Count)
+            {
+                shortest = first;
+                longest = second;
+            }
+            else
+            {
+                shortest = second;
+                longest = first;
+            }
+            Element match = null;
+            LinkedListNode<Element> elementShort = shortest.First;
+            LinkedListNode<Element> elementLongest = longest.First;
+            for(int i = 0; i< diff; i++)
+            {
+                elementLongest = elementLongest.Next;
+            }
+            while (elementShort != null)
+            {
+                if (elementShort.Value.ID == elementLongest.Value.ID)
+                {
+                    if (match == null)
+                    {
+                        match = elementLongest.Value;
+                    }
+                }
+                else
+                {
+                    match = null;
+                }
+                elementShort = elementShort.Next;
+                elementLongest = elementLongest.Next;
+            }
+            return match;
+           
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge19.cs
@@ -0,0 +1,50 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge19;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge19
+    {
+        [TestMethod]
+        public void DailyChallenge19()
+        {
+            DailyChallenge19 daily = new DailyChallenge19();
+            Element[] common = new Element[]
+            {
+                new Element(8), new Element(10)
+            };
+            List<Element> first = new List<Element>()
+            {
+                new Element(3),
+                new Element(7)
+            };
+            first.AddRange(common);
+            List<Element> second = new List<Element>()
+            {
+                new Element(99), new Element(1)
+            };
+            second.AddRange(common);
+            Assert.AreEqual(common[0], daily.Proceed(new LinkedList<Element>(first), new LinkedList<Element>(second)));
+
+            common = new Element[]
+            {
+                new Element(7), new Element(8), new Element(10)
+            };
+            first = new List<Element>() { new Element(101), new Element(52), new Element(31), new Element(5), new Element(6) };
+            second = new List<Element>() { new Element(53), new Element(42), new Element(2) };
+            first.AddRange(common);
+            second.AddRange(common);
+
+            Assert.AreEqual(common[0], daily.Proceed(new LinkedList<Element>(first), new LinkedList<Element>(second)));
+
+
+        }
+    }
+}
