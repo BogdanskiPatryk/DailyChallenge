510a6424499bef5b127a19929e472c4e37b57ad2
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-01 21:02:39 +0100

    daily20

---
+++ Modules/DailyChallenge/DailyChallenge20.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge20
+    {
+        private class Room
+        {
+            public List<Lecture> Lectures = new List<Lecture>();
+        }
+
+        private class Lecture
+        {
+            public int Start;
+            public int End;
+        }
+
+        public int Proceed(IEnumerable<Tuple<int,int>> data)
+        {
+            IEnumerable<Tuple<int, int>> sorted = data.OrderBy(x => x.Item1);
+            List<Room> rooms = new List<Room>();
+            foreach (var lecture in sorted)
+            {
+                if (TryAddForExistingRoom(rooms, lecture))
+                {
+                    continue;
+                }
+                Room room = new Room();
+                room.Lectures.Add(new Lecture { Start = lecture.Item1, End = lecture.Item2 });
+                rooms.Add(room);
+            }
+            return rooms.Count;
+        }
+
+        private bool TryAddForExistingRoom(List<Room> rooms, Tuple<int, int> lecture)
+        {
+            foreach (var room in rooms)
+            {
+                Lecture lastLect = room.Lectures.Last();
+                if (lecture.Item1 >= lastLect.End)
+                {
+                    // Add on end
+                    room.Lectures.Add(new Lecture { Start = lecture.Item1, End = lecture.Item2 });
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge20.cs
@@ -0,0 +1,36 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge20
+    {
+        [TestMethod]
+        public void DailyChallenge20()
+        {
+            DailyChallenge20 daily = new DailyChallenge20();
+            Assert.AreEqual(2, daily.Proceed(new Tuple<int, int>[]
+            {
+                new Tuple<int, int>(30,75),
+                new Tuple<int, int>(0,50),
+                new Tuple<int, int>(60,150),
+            }));
+
+            Assert.AreEqual(3, daily.Proceed(new Tuple<int, int>[]
+            {
+                new Tuple<int, int>(10,15),
+                new Tuple<int, int>(0,20),
+                new Tuple<int, int>(15,25),
+                new Tuple<int, int>(25,50),
+                new Tuple<int, int>(5,25),
+                new Tuple<int, int>(20,40),
+            }));
+        }
+    }
+}
