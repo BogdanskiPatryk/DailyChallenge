fbcfff1ad7d29a0ca135510957cfc6e744896c0a
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-03 14:15:34 +0100

    daily 21

---
+++ Modules/DailyChallenge/DailyChallenge21.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge21
+    {
+        public string[] Proceed(string strText, IEnumerable<string> set)
+        {
+            LinkedList<string> result = new LinkedList<string>();
+            if (ProceedIntern(strText, set.ToList(), result))
+            {
+                return result.ToArray();
+            }
+            return null;
+        }
+
+        private bool ProceedIntern(string strText, List<string> leftWords, LinkedList<string> result)
+        {
+            if (strText.Length == 0)
+            {
+                return true;
+            }
+            foreach(string word in leftWords)
+            {
+                if (strText.StartsWith(word))
+                {
+                    result.AddLast(word);
+                    if (ProceedIntern(strText.Substring(word.Length), leftWords.Except(new string[] { word}).ToList(), result))
+                    {
+                        return true;
+                    }
+                    result.RemoveLast();
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge21.cs
@@ -0,0 +1,56 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge21
+    {
+        [TestMethod]
+        public void DailyChallenge21()
+        {
+            DailyChallenge21 daily = new DailyChallenge21();
+            string[] result = daily.Proceed("thequickbrownfox", new string[] { "quick", "brown", "the", "fox" });
+            Assert.IsTrue(IsEqual(new string[] { "the", "quick", "brown", "fox" }, result));
+            result = daily.Proceed("bedbathandbeyond", new string[] { "bed", "bath", "bedbath", "and", "beyond" });
+            Assert.IsTrue(IsAnyEqual(new List<string[]>
+            {
+                new string[] { "bed", "bath", "and", "beyond" },
+                new string[] { "bedbath", "and", "beyond" }
+            }, result));
+        }
+
+        private bool IsEqual(string[] expected, string[] current)
+        {
+            if (expected.Length != current.Length)
+            {
+                return false;
+            }
+            for(int i = 0; i< expected.Length;++i)
+            {
+                if (expected[i] != current[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsAnyEqual(IEnumerable<string[]> expected, string[] current)
+        {
+            foreach(var e in expected)
+            {
+                if (IsEqual(e, current))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
