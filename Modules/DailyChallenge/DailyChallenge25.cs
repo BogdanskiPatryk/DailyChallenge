b9494cbc94f8b1bf3f199bee6e4c1c110b3f3cde
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-08 20:01:41 +0100

    Daily25

---
+++ Modules/DailyChallenge/DailyChallenge25.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge25
+    {
+        /*
+         * Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+The list is very long, so making more than one pass is prohibitively expensive.
+Do this in constant space and in one pass.
+
+         */
+        public class Node
+        {
+            public Guid ID = Guid.NewGuid();
+            public Node Next;
+        }
+
+
+        public void Proceed(Node first, int kLast)
+        {
+            Node node = first;
+            Node remove = first;
+            int k = 0;
+            while (node != null)
+            {
+                if (k > kLast)
+                {
+                    remove = remove.Next;
+                }
+                k++;
+                node = node.Next;
+            }
+            // GC will do the rest
+            remove.Next = null;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge25.cs
@@ -0,0 +1,50 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge25;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge25
+    {
+        [TestMethod]
+        public void DailyChallenge25()
+        {
+            DailyChallenge25 daily = new DailyChallenge25();
+            Node node400 = GenerateRandom(400);
+            daily.Proceed(node400,100);
+            int i = 0;
+            while (node400 != null)
+            {
+                node400 = node400.Next;
+                ++i;
+            }
+            Assert.AreEqual(300, i);
+        }
+        
+        private Node GenerateRandom(int k)
+        {
+            Node first = null;
+            Node last = null;
+            for(int i=0;i<k;++i)
+            {
+                Node newNode = new Node();
+                if (last != null)
+                {
+                    last.Next = newNode;
+                }
+                else
+                {
+                    first = newNode;
+                }
+                last = newNode;
+            }
+            return first;
+        }
+    }
+}
