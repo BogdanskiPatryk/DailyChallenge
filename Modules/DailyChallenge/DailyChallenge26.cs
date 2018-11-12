86285458a6f1a54f943c34381ea6ee9c610e5f98
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-12 11:59:46 +0100

    daily 26

---
+++ Modules/DailyChallenge/DailyChallenge26.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge26
+    {
+        /*
+         * iven a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+For example, given the string "([])[]({})", you should return true.
+Given the string "([)]" or "((()", you should return false.
+         */
+         
+        private class OpenClose
+        {
+            public char Open;
+            public char Close;
+        }
+
+        private List<OpenClose> _openClose = new List<OpenClose>()
+        {
+            new OpenClose{Open = '{', Close = '}'},
+            new OpenClose{Open = '[', Close = ']'},
+            new OpenClose{Open = '(', Close = ')'},
+        };
+
+        public bool Proceed(string text)
+        {
+            Stack<char> queue = new Stack<char>();
+            foreach(var ch in text)
+            {
+                OpenClose open = _openClose.FirstOrDefault(x => x.Open == ch);
+                OpenClose close = _openClose.FirstOrDefault(x => x.Close == ch);
+                if (open == null && close == null)
+                {
+                    return false;
+                }
+                if (open != null)
+                {
+                    queue.Push(open.Open);
+                }
+                else
+                {
+                    if (queue.Any() == false)
+                    {
+                        return false;
+                    }
+                    char poped = queue.Pop();
+                    if (poped != close.Open)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return queue.Any() == false;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge26.cs
@@ -0,0 +1,23 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge26
+    {
+        [TestMethod]
+        public void DailyChallenge26()
+        {
+            DailyChallenge26 daily = new DailyChallenge26();
+            Assert.IsTrue(daily.Proceed("([])[]({})"));
+            Assert.IsFalse(daily.Proceed("((()"));
+            Assert.IsFalse(daily.Proceed("([)]"));
+        }
+    }
+}
