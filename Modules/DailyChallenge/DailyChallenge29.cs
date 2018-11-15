cb9d3a1336a1e81294739e380ad5f274b82b40f9
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-15 21:01:01 +0100

    daily 29

---
+++ Modules/DailyChallenge/DailyChallenge29.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge29
+    {
+        /*
+         * You are given an array of non-negative integers that represents a two-dimensional elevation map where each element is unit-width wall and the integer is the height. Suppose it will rain and all spots between two walls get filled up.
+Compute how many units of water remain trapped on the map in O(N) time and O(1) space.
+For example, given the input [2, 1, 2], we can hold 1 unit of water in the middle.
+Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
+         */
+
+
+        //public int Proceed(int[] map)
+        //{
+        //    int result = 0;
+        //    int start = 0;
+        //    int max = map[0];
+        //    for (int i = 1; i < map.Length; ++i)
+        //    {
+        //        if (map[i] <= map[i - 1])
+        //        {
+        //            continue;
+        //        }
+        //        if (start == i - 1 && map[i] >= max)
+        //        {
+        //            max = map[i];
+        //            start = i;
+        //            continue;
+        //        }
+        //        int maximum = Math.Min(max, map[i]);
+        //        for (int k = i - 1; k >= start; --k)
+        //        {
+        //            if (map[k] < maximum)
+        //            {
+        //                result += maximum - map[k];
+        //                map[k] = maximum;
+        //                continue;
+        //            }
+        //            break;
+        //        }
+        //        if (max < map[i])
+        //        {
+        //            max = map[i];
+        //            start = i;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        public int Proceed(int[] map)
+        {
+            int result = 0;
+            int lMax = 0;
+            int rMax = 0;
+            int left = 0;
+            int right = map.Length - 1;
+
+            while (left <= right)
+            {
+                if (map[left] < map[right])
+                {
+                    if (map[left] > lMax)
+                    {
+                        lMax = map[left];
+                    }
+                    else
+                    {
+                        result += lMax - map[left];
+                    }
+                    left++;
+                }
+                else
+                {
+                    if (map[right] > rMax)
+                    {
+                        rMax = map[right];
+                    }
+                    else
+                    {
+                        result += rMax - map[right];
+                    }
+                    right--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge29.cs
@@ -0,0 +1,56 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge29
+    {
+        [TestMethod]
+        public void DailyChallenge29()
+        {
+            DailyChallenge29 daily = new DailyChallenge29();
+            Assert.AreEqual(1, daily.Proceed(new int[] { 2, 1, 2 }));
+            Assert.AreEqual(8, daily.Proceed(new int[] { 3, 0, 1, 3, 0, 5 }));
+            Assert.AreEqual(0, daily.Proceed(new int[] { 2,3,4,5,6,5,4,3,2}));
+            Assert.AreEqual(30, daily.Proceed(new int[] { 4, 3, 0, 0, 3, 0, 0, 2, 0, 0, 2, 4 }));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void DailyChallenge29_Performance()
+        {
+            int[] len = new int[] { 100000, 1000000};
+            DailyChallenge29 daily = new DailyChallenge29();
+            Stopwatch[] sws = new Stopwatch[2]
+            {
+                new Stopwatch(), new Stopwatch() 
+            };
+            for(int l = 0; l <len.Length; ++l)
+            {
+                for (int k = 0; k < 3; ++k)
+                {
+                    int value = len[l] / 2;
+                    int[] test = new int[len[l]];
+                    for (int i = 0; i < test.Length; i++)
+                    {
+                        test[i] = Math.Abs(value);
+                        value--;
+                    }
+                    sws[l].Start();
+                    daily.Proceed(test);
+                    sws[l].Stop();
+                }
+            }
+            Trace.WriteLine($"For 100000: {sws[0].ElapsedTicks / 3}");
+            Trace.WriteLine($"For 1000000: {sws[1].ElapsedTicks / 3}");
+
+        }
+    }
+}
