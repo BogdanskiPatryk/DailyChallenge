3afe23b4d3fbe73c5ff99ea5b064a802b758120f
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-20 20:23:20 +0100

    daily32

---
+++ Modules/DailyChallenge/DailyChallenge32.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge32
+    {
+        /*
+         * Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
+Recall that the median of an even-numbered list is the average of the two middle numbers.
+For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
+2
+1.5
+2
+3.5
+2
+2
+2
+
+         */
+
+        public float[] Proceed(int[] numbers)
+        {
+            List<float> result = new List<float>();
+            List<int> listOfNumbers = new List<int>();
+            foreach(int number in numbers)
+            {
+                listOfNumbers.Add(number);
+                listOfNumbers.Sort();
+                if (listOfNumbers.Count % 2 == 0)
+                {
+                    // even
+                    float res = (listOfNumbers[listOfNumbers.Count/2 -1] + listOfNumbers[listOfNumbers.Count / 2]) / 2f;
+                    result.Add(res);
+                }
+                else
+                {
+                    result.Add(listOfNumbers[listOfNumbers.Count / 2]);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge32.cs
@@ -0,0 +1,27 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge32
+    {
+        [TestMethod]
+        public void DailyChallenge32()
+        {
+            DailyChallenge32 daily = new DailyChallenge32();
+            float[] result = daily.Proceed(new int[] { 2, 1, 5, 7, 2, 0, 5 });
+            float[] expected = new float[] { 2, 1.5f, 2, 3.5f, 2, 2, 2 };
+            Assert.AreEqual(expected.Length, result.Length);
+            for(int i =0;i<result.Length;++i)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
