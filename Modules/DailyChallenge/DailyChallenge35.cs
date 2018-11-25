1ecf87af050386a25af70aaefece3e34dd83b165
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-25 18:52:15 +0100

    Daily 35

---
+++ Modules/DailyChallenge/DailyChallenge35.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge35
+    {
+        public class Node
+        {
+            public Node Left;
+            public Node Right;
+            public int Value;
+        }
+
+        public int Proceed(Node root)
+        {
+            return ProceedIntern(root, null)?.Value ?? -1;
+        }
+
+        private Node ProceedIntern(Node current, Node secondLargest)
+        {
+            if (current.Right != null)
+            {
+                secondLargest = current;
+                return ProceedIntern(current.Right, secondLargest);
+            }
+            else if (current.Left != null)
+            {
+                secondLargest = current.Left;
+                return ProceedIntern(current.Left, secondLargest);
+            }
+            return secondLargest;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge35.cs
@@ -0,0 +1,51 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge35;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge35
+    {
+        [TestMethod]
+        public void DailyChallenge35()
+        {
+            DailyChallenge35 daily = new DailyChallenge35();
+            Node root = new Node
+            {
+                Value = 8,
+                Left = new Node
+                {
+                    Value = 3,
+                    Left = new Node()
+                    {
+                        Value = 1
+                    },
+                    Right = new Node()
+                    {
+                        Value = 6,
+                        Right = new Node { Value = 7},
+                        Left = new Node { Value = 4}
+                    }
+                },
+                Right = new Node()
+                {
+                    Value = 10,
+                    Right = new Node
+                    {
+                        Value = 14,
+                        Left = new Node { Value= 13}
+                    }
+
+
+                }
+            };
+            Assert.AreEqual(13, daily.Proceed(root));
+        }
+    }
+}
