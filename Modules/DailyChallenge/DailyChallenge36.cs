8fba52d843a19b634223079b243528323a9b4b0c
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-11-27 19:35:16 +0100

    daily36

---
+++ Modules/DailyChallenge/DailyChallenge36.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge36
+    {
+        /*
+         * The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+You may also use a list or array to represent a set.
+
+         */
+
+        public List<List<int>> Proceed(IEnumerable<int> input)
+        {
+            return ProceedIntern(input);
+        }
+
+        private List<List<int>> ProceedIntern(IEnumerable<int> input)
+        {
+            List<List<int>> result = new List<List<int>>();
+            int count = input.Count();
+            for (int i = 0; i < count; ++i)
+            {
+                foreach(var rest in ProceedIntern(input.Skip(i+1).ToList()))
+                {
+                    List<int> res = new List<int>() { input.ElementAt(i) };
+                    res.AddRange(rest);
+                    result.Add(res);
+                }
+            }
+            result.Add(new List<int>());
+            return result;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge36.cs
@@ -0,0 +1,30 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge36
+    {
+        [TestMethod]
+        public void DailyChallenge36()
+        {
+            DailyChallenge36 daily = new DailyChallenge36();
+            List<List<int>> result = daily.Proceed(new int[] { 1, 2, 3 });
+            Assert.AreEqual(8, result.Count);
+            Assert.IsTrue(result.Any(x => x.Count == 0));
+            Assert.IsTrue(result.Any(x => x.Count == 1 && x[0] == 1));
+            Assert.IsTrue(result.Any(x => x.Count == 1 && x[0] == 1));
+            Assert.IsTrue(result.Any(x => x.Count == 1 && x[0] == 1));
+            Assert.IsTrue(result.Any(x => x.Count == 2 && x[0] == 1 && x[1] == 2));
+            Assert.IsTrue(result.Any(x => x.Count == 2 && x[0] == 1 && x[1] == 3));
+            Assert.IsTrue(result.Any(x => x.Count == 2 && x[0] == 1 && x[1] == 2));
+            Assert.IsTrue(result.Any(x => x.Count == 3 && x[0] == 1 && x[1] == 2 && x[2] == 3));
+        }
+    }
+}
