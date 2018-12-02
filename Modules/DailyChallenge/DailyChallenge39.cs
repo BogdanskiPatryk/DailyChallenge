a9e28a5b53322f2d4aaf066f5b887dedbc32d667
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-02 18:44:02 +0100

    Daily39

---
+++ Modules/DailyChallenge/DailyChallenge39.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge39
+    {
+        /*
+         * Given an array of integers where every integer occurs three times except for one integer, which only occurs once, find and return the non-duplicated integer.
+For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+Do this in O(N) time and O(1) space.
+         */
+
+        public int Proceed(int[] input)
+        {
+            // size of int in C#
+            int maxBits = 32;
+            int result = 0;
+            for (int i = 0; i< maxBits;++i)
+            {
+                int sum = 0;
+                int current = (1 << i);
+                foreach (int value in input)
+                {
+                    if ((value & current) == 0)
+                    {
+                        sum++;
+                    }
+                }
+                if ((sum % 3) == 0)
+                {
+                    result |= current;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge39.cs
@@ -0,0 +1,26 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge39
+    {
+        [TestMethod]
+        public void DailyChallenge39()
+        {
+            DailyChallenge39 daily = new DailyChallenge39();
+            Assert.AreEqual(19, daily.Proceed(new[] { 13, 13, 19, 13 }));
+            Assert.AreEqual(1, daily.Proceed(new[] { 6, 1, 3, 3, 3, 6, 6 }));
+            Assert.AreEqual(-3, daily.Proceed(new[] { -2, -2, -2, 1, 1, 1, -3 }));
+            Assert.AreEqual(int.MaxValue, daily.Proceed(new[] { int.MinValue, int.MinValue, int.MinValue, int.MaxValue}));
+            Assert.AreEqual(int.MinValue, daily.Proceed(new[] { int.MaxValue, int.MaxValue, int.MinValue, int.MaxValue }));
+            Assert.AreEqual(int.MinValue + 1, daily.Proceed(new[] { int.MaxValue, int.MaxValue, int.MinValue+1, int.MinValue, int.MaxValue, int.MinValue, int.MinValue }));
+        }
+    }
+}
