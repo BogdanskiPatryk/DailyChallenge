26b48683136f1dd9645cdb53fef5f93f60f893d3
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-03 18:38:54 +0100

    daily41

---
+++ Modules/DailyChallenge/DailyChallenge41.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge41
+    {
+        /*
+        Given a list of integers S and a target number k, write a function that returns a subset of S that adds up to k. If such a subset cannot be made, then return null.
+Integers can appear more than once in the list. You may assume all numbers in the list are positive.
+For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
+         */
+
+        public int[] Proceed(int[] input, int k)
+        {
+            List<int> result = new List<int>();
+            if (ProceedIntern(result, input, k))
+            {
+                return result.ToArray();
+            }
+            return null;
+        }
+
+        private bool ProceedIntern(List<int> result, int[] input, int k)
+        {
+            foreach(int i in input.Where(x => x <= k))
+            {
+                int res = k - i;
+                if (res == 0)
+                {
+                    result.Add(i);
+                    return true;
+                }
+                result.Add(i);
+                if (ProceedIntern(result, PrepareCollection(input, i, res), res))
+                {
+                    return true;
+                }
+                result.RemoveAt(result.Count - 1);
+            }
+            return false;
+        }
+
+        private int[] PrepareCollection(int[] current, int currentItem, int k)
+        {
+            List<int> result = new List<int>();
+            bool bFirst = true;
+            foreach(int i in current)
+            {
+                if (i > k)
+                {
+                    continue;
+                }
+                if (i == currentItem && bFirst)
+                {
+                    bFirst = false;
+                    continue;
+                }
+                result.Add(i);
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge41.cs
@@ -0,0 +1,21 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge41
+    {
+        [TestMethod]
+        public void DailyChallenge41()
+        {
+            DailyChallenge41 daily = new DailyChallenge41();
+            CollectionAssert.AreEqual(new int[] { 12, 9, 2, 1 }, daily.Proceed(new int[] { 12, 1, 61, 5, 9, 2 }, 24).OrderByDescending(x => x).ToArray());
+        }
+    }
+}
