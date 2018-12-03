4868978de031cca1db89153f94d10cb6384d3d45
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-03 19:17:39 +0100

    Daily42

---
+++ Modules/DailyChallenge/DailyChallenge42.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge42
+    {
+        /*
+         * Implement a stack that has the following methods:
+•	push(val), which pushes an element onto the stack
+•	pop(), which pops off and returns the topmost element of the stack. If there are no elements in the stack, then it should throw an error or return null.
+•	max(), which returns the maximum value in the stack currently. If there are no elements in the stack, then it should throw an error or return null.
+Each method should run in constant time.
+         */
+
+
+        private class Element
+        {
+            public int Value;
+            public int Max;
+        }
+
+        private LinkedList<Element> _elements = new LinkedList<Element>();
+
+        public void Push(int value)
+        {
+            if (_elements.Count == 0)
+            {
+                _elements.AddLast(new Element() { Value = value, Max = value });
+            }
+            else
+            {
+                _elements.AddLast(new Element() { Value = value, Max = Math.Max(value, _elements.Last.Value.Max) }); 
+            }
+        }
+
+        public int Pop()
+        {
+            if(_elements.Last == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Element element = _elements.Last.Value;
+            _elements.RemoveLast();
+            return element.Value;
+        }
+
+        public int Max()
+        {
+            if (_elements.Last == null)
+            {
+                throw new ArgumentNullException();
+            }
+            return _elements.Last.Value.Max;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge42.cs
@@ -0,0 +1,35 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge42
+    {
+        [TestMethod]
+        public void DailyChallenge42()
+        {
+            DailyChallenge42 daily = new DailyChallenge42();
+            daily.Push(1);
+            Assert.AreEqual(1, daily.Max());
+            daily.Push(-2);
+            Assert.AreEqual(1, daily.Max());
+            daily.Push(5);
+            Assert.AreEqual(5, daily.Max());
+            daily.Push(15);
+            Assert.AreEqual(15, daily.Max());
+            Assert.AreEqual(15, daily.Pop());
+            Assert.AreEqual(5, daily.Max());
+            Assert.AreEqual(5, daily.Pop());
+            Assert.AreEqual(1, daily.Max());
+            Assert.AreEqual(-2, daily.Pop());
+            Assert.AreEqual(1, daily.Max());
+            Assert.AreEqual(1, daily.Pop());
+        }
+    }
+}
