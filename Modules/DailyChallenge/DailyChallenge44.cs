46dbdc82b0b186848c79c504d8f61716d24f0a60
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-10 20:09:43 +0100

    Daily44

---
+++ Modules/DailyChallenge/DailyChallenge44.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge44
+    {
+        /*
+         * Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a function rand7() that returns an integer from 1 to 7 (inclusive).
+         */
+
+        public int Proceed()
+        {
+            // 7*5 
+            // Min = 7, Max = 35
+            int rand = Rand5();
+            rand += Rand5();
+            rand += Rand5();
+            rand += Rand5();
+            rand += Rand5();
+            rand += Rand5();
+            rand += Rand5();
+            return rand % 7 + 1;
+        }
+
+        private int Rand5()
+        {
+            return new Random((int)DateTime.Now.Ticks).Next(1, 6);
+        }
+    }
+}
