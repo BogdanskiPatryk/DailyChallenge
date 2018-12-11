e51c7474f259e1a84916b2622cda822ddc86e851
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-11 20:18:12 +0100

    Daily45

---
+++ Modules/DailyChallenge/DailyChallenge45.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge45
+    {
+        /*
+         * Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum length, return any one.
+For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
+         */
+
+        public string Proceed(string text)
+        {
+            if (IsPalindrom(text))
+            {
+                return text;
+            }
+            return ProceedIntern(text, null);
+        }
+
+        private string ProceedIntern(string text, bool? onRight)
+        {
+            if (text.Length <= 1)
+            {
+                return text;
+            }
+            string strLeft = text.Substring(0, text.Length - 1);
+            if (IsPalindrom(strLeft))
+            {
+                return strLeft;
+            }
+            string strRight = text.Substring(1, text.Length - 1);
+            if (IsPalindrom(strRight))
+            {
+                return strRight;
+            }
+            string rightPalindrom = string.Empty;
+            string leftPalindrom = string.Empty;
+            if (onRight.HasValue == false || onRight.Value)
+            {
+                rightPalindrom = ProceedIntern(strRight, true);
+            }
+            if (onRight.HasValue == false || onRight.Value == false)
+            {
+                leftPalindrom = ProceedIntern(strLeft, false);
+            }
+            return leftPalindrom.Length > rightPalindrom.Length ? leftPalindrom : rightPalindrom;
+        }
+
+        private bool IsPalindrom(string text)
+        {
+            int l = 0;
+            int h = text.Length - 1;
+            while (text[l] == text[h])
+            {
+                if (l >= h)
+                {
+                    // palindrom
+                    return true;
+                }
+                l++;
+                h--;
+            }
+            return false;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge45.cs
@@ -0,0 +1,22 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge45
+    {
+        [TestMethod]
+        public void DailyChallenge45()
+        {
+            DailyChallenge45 daily = new DailyChallenge45();
+            Assert.AreEqual("bcdcb", daily.Proceed("aabcdcb"));
+            Assert.AreEqual("anana", daily.Proceed("bananas"));
+        }
+    }
+}
