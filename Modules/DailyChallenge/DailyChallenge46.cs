385df1655aad082ff1821a5b0585b9f2be2acdae
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-12 19:36:22 +0100

    daily46

---
+++ Modules/DailyChallenge/DailyChallenge46.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge46
+    {
+        /*
+         * Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+         */
+
+        public int Proceed(IEnumerable<int> prices)
+        {
+            if (prices.Count() <= 1)
+            {
+                return 0;
+            }
+            int first = prices.ElementAt(0);
+            int max = 0;
+            foreach(int price in prices.Skip(1))
+            {
+                max = Math.Max(max, price - first);
+            }
+            return Math.Max(max, Proceed(prices.Skip(1)));
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge46.cs
@@ -0,0 +1,21 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge46
+    {
+        [TestMethod]
+        public void DailyChallenge46()
+        {
+            DailyChallenge46 daily = new DailyChallenge46();
+            Assert.AreEqual(5, daily.Proceed(new int[] { 9, 11, 8, 5, 7, 10 }));
+        }
+    }
+}
