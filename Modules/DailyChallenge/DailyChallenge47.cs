176833e557e38a8035f9964b6ba8cf5fcedcf6bf
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-13 20:15:41 +0100

    Daily47

---
+++ Modules/DailyChallenge/DailyChallenge47.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge47
+    {
+        /*
+         * Given pre-order and in-order traversals of a binary tree, write a function to reconstruct the tree.
+For example, given the following preorder traversal:
+[a, b, d, e, c, f, g]
+And the following inorder traversal:
+[d, b, e, a, f, c, g]
+You should return the following tree:
+    a
+   / \
+  b   c
+ / \ / \
+d  e f  g
+         */
+
+        public class Node
+        {
+            public Node Left;
+            public Node Right;
+            public char Value;
+        }
+
+        public Node Proceed(char[] preorder, char[] inorder)
+        {
+            if (preorder.Length != inorder.Length)
+            {
+                throw new ArgumentException();
+            }
+            Node root = new Node();
+            if(ProceedIntern(root, preorder.ToList(), inorder.ToList()))
+            {
+                return root;
+            }
+            return null;
+        }
+
+        public bool ProceedIntern(Node current, List<char> preorder, List<char> inorder)
+        {
+            if (inorder.Any() == false)
+            {
+                return false;
+            }
+            current.Value = preorder[0];
+            if (inorder.Count == 2)
+            {
+                current.Left = new Node() { Value = preorder[1] };
+            }
+            else if (inorder.Count == 3)
+            {
+                current.Left = new Node() { Value = preorder[1] };
+                current.Right = new Node() { Value = preorder[2] };
+            }
+            else
+            {
+                int index = inorder.IndexOf(current.Value);
+                if (index < 0)
+                {
+                    throw new InvalidOperationException();
+                }
+                Node left = new Node();
+                if (ProceedIntern(left, preorder.Skip(1).ToList(), inorder.Take(index).ToList()))
+                {
+                    current.Left = left;
+                }
+                Node right = new Node();
+                if (ProceedIntern(right, preorder.Skip(index + 1).ToList(), inorder.Skip(index + 1).ToList()))
+                {
+                    current.Right = right;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge47.cs
@@ -0,0 +1,55 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge47;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge47
+    {
+        /*
+         * [a, b, d, e, c, f, g]
+And the following inorder traversal:
+[d, b, e, a, f, c, g]
+You should return the following tree:
+    a
+   / \
+  b   c
+ / \ / \
+d  e f  g
+
+         */
+
+        [TestMethod]
+        public void DailyChallenge47()
+        {
+            DailyChallenge47 daily = new DailyChallenge47();
+            Node root = daily.Proceed(new char[] { 'a', 'b', 'd', 'e', 'c', 'f', 'g' }, new char[] { 'd', 'b', 'e', 'a', 'f', 'c', 'g' });
+            Assert.IsNotNull(root);
+            Assert.AreEqual('a', root.Value);
+
+            Assert.IsNotNull(root.Left);
+            Assert.AreEqual('b', root.Left.Value);
+
+            Assert.IsNotNull(root.Right);
+            Assert.AreEqual('c', root.Right.Value);
+
+            Assert.IsNotNull(root.Left.Left);
+            Assert.AreEqual('d', root.Left.Left.Value);
+
+            Assert.IsNotNull(root.Left.Right);
+            Assert.AreEqual('e', root.Left.Right.Value);
+
+            Assert.IsNotNull(root.Right.Left);
+            Assert.AreEqual('f', root.Right.Left.Value);
+
+            Assert.IsNotNull(root.Right.Right);
+            Assert.AreEqual('g', root.Right.Right.Value);
+        }
+    }
+}
