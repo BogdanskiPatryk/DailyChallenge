ea51a0ae31a64637f6532a3e72305232b046e26e
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-14 19:09:26 +0100

    Daily49

---
+++ Modules/DailyChallenge/DailyChallenge49.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    public class DailyChallenge49
+    {
+        /*
+         * Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+Given the root to such a tree, write a function to evaluate it.
+For example, given the following tree:
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+You should return 45, as it is (3 + 2) * (4 + 5).
+         */
+         public class Node
+        {
+            public Node Left;
+            public Node Right;
+            public string Value;
+        }
+
+        public double Proceed(Node root)
+        {
+            if (string.IsNullOrWhiteSpace(root?.Value))
+            {
+                throw new ArgumentNullException();
+            }
+            if (double.TryParse(root.Value, out double value))
+            {
+                return value;
+            }
+            if (root.Value.Length > 1)
+            {
+                throw new ArgumentException();
+            }
+            char ch = root.Value[0];
+            if (ch == '+')
+            {
+                return Proceed(root.Left) + Proceed(root.Right);
+            }
+            else if (ch == '-')
+            {
+                return Proceed(root.Left) - Proceed(root.Right);
+            }
+            else if (ch == '*')
+            {
+                return Proceed(root.Left) * Proceed(root.Right);
+            }
+            else if (ch == '/')
+            {
+
+                return Proceed(root.Left) / Proceed(root.Right);
+            }
+            throw new ArgumentException();
+        }
+
+
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge49.cs
@@ -0,0 +1,38 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DailyChallenge.DailyChallenge49;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge49
+    {
+        [TestMethod]
+        public void DailyChallenge49()
+        {
+            DailyChallenge49 daily = new DailyChallenge49();
+            Node root = new Node()
+            {
+                Value = "*",
+                Left = new Node()
+                {
+                    Value = "+",
+                    Left = new Node() { Value = "3" },
+                    Right = new Node() { Value = "2" }
+                },
+                Right = new Node()
+                {
+                    Value = "+",
+                    Left = new Node() { Value = "4" },
+                    Right = new Node() { Value = "5" }
+                }
+            };
+            Assert.AreEqual(45, daily.Proceed(root));
+        }
+    }
+}
