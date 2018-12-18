ce89e96e3970b56b70b94a289f1aef4c364290f1
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-18 19:48:28 +0100

    Daily52

---
+++ Modules/DailyChallenge/DailyChallenge52.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyChallenge
+{
+    /*
+     * Implement a queue using two stacks. Recall that a queue is a FIFO (first-in, first-out) data structure with the following methods: enqueue, which inserts an element into the queue, and dequeue, which removes it.
+     */
+    public class DailyChallenge52
+    {
+        private Stack<int> _first = new Stack<int>();
+        private Stack<int> _second = new Stack<int>();
+
+        public void Enqueue(int value)
+        {
+            _first.Push(value);
+        }
+
+        public int Dequeue()
+        {
+            if (_first.Any() == false)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            while(_first.Any())
+            {
+                _second.Push(_first.Pop());
+            }
+            int result = _second.Pop();
+            while(_second.Any())
+            {
+                _first.Push(_second.Pop());
+            }
+            return result;
+        }
+    }
+}
---
+++ Modules/UnitTests.DailyChallenge/TestDailyChallenge52.cs
@@ -0,0 +1,27 @@
+﻿using DailyChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.DailyChallenge
+{
+    [TestClass]
+    public class TestDailyChallenge52
+    {
+        [TestMethod]
+        public void DailyChallenge52()
+        {
+            DailyChallenge52 daily = new DailyChallenge52();
+            daily.Enqueue(1);
+            daily.Enqueue(2);
+            daily.Enqueue(3);
+            Assert.AreEqual(1, daily.Dequeue());
+            Assert.AreEqual(2, daily.Dequeue());
+            Assert.AreEqual(3, daily.Dequeue());
+        }
+        
+    }
+}
